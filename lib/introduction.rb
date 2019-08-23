96c14b3cab62818b74902866d570633b65212450
Bruno de Oliveira <corporate.zanin@gmail.com>
2019-08-23 11:41:12 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,15 +1,16 @@
 def introduction(name)
   puts "Hi, my name is #{name}"
 end 
-introduction("Bruno")
+
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
-introduction_with_language("Bruno", "Ruby")
+
 def introduction_with_language_optional(name, language = "Ruby")
   puts "Hi, my name is #{name}and i am learning to program in #{language}"
 end
-introduction_with_language_optional("Bruno")
+
+
 
 
 
